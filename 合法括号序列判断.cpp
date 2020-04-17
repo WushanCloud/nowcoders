c71db98cab3735fc11f61117542d6e2b8e55ce84
WushanCloud <55572755+WushanCloud@users.noreply.github.com>
2020-04-17 16:48:04 +0800

    Create 合法括号序列判断.cpp

---
+++ 合法括号序列判断.cpp
@@ -0,0 +1,39 @@
+
+对于一个字符串，请设计一个算法，判断其是否为一个合法的括号串。
+
+给定一个字符串A和它的长度n，请返回一个bool值代表它是否为一个合法的括号串。
+
+测试样例：
+"(()())",6
+返回：true
+测试样例：
+"()a()()",7
+返回：false
+测试样例：
+"()(()()",7
+返回：false
+
+
+class Parenthesis {
+public:
+    bool chkParenthesis(string A, int n) {
+        // write code here
+        string str = "()";
+        string B;
+        for (int i = 0; i < A.size(); i++)
+        {
+            if (A[i] == str[0])
+                B.push_back(A[i]);
+            else if (A[i] == str[1])
+            {
+                if (B.size() > 0)
+                    B.pop_back();
+                else
+                    return false;
+            }
+        }
+        if (B.size() == 0)
+            return true;
+        return false;
+    }
+};
