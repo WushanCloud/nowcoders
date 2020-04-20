9675fb5f53e570c475b574c25071e03ec540d033
WushanCloud <55572755+WushanCloud@users.noreply.github.com>
2020-04-20 22:15:34 +0800

    Create 用两个栈实现队列.cpp

---
+++ 用两个栈实现队列.cpp
@@ -0,0 +1,30 @@
+用两个栈来实现一个队列，完成队列的Push和Pop操作。 队列中的元素为int类型。
+
+class Solution
+{
+public:
+    void push(int node) {
+        stack1.push(node);
+    }
+
+    int pop() {
+        int a;
+        while(stack1.size()!= 1)
+        {
+            stack2.push(stack1.top());
+            stack1.pop();
+        }
+        a =  stack1.top();
+        stack1.pop();
+        while(!stack2.empty())
+        {
+            stack1.push(stack2.top());
+            stack2.pop();
+        }
+        return a;
+    }
+
+private:
+    stack<int> stack1;
+    stack<int> stack2;
+};
